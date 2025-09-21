cb89cae767dfeb13a19cfe0069c7db0fa24b345b
lopez <14julianlopez@gmail.com>
2025-09-20 20:05:50 -0500

    Estructura sin-framework

---
+++ sin-framework/js/contacto/domain/Contacto.js
@@ -0,0 +1,13 @@
+class Contacto {
+    constructor(nombre,email,telefono,motivo,mensaje,aceptaTermino,preferenciaContacto,fechaCreacion,fechaActualizacion) {
+        this.nombre = nombre;
+        this.email = email;
+        this.telefono = telefono;
+        this.motivo = motivo;
+        this.mensaje = mensaje;
+        this.aceptaTermino = aceptaTermino;
+        this.preferenciaContacto = preferenciaContacto;
+        this.fechaCreacion = fechaCreacion;
+        this.fechaActualizacion = fechaActualizacion;
+    }
+}
